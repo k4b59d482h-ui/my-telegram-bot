f4037f9d649e097975c7765301a4ea401c2c277f
k4b59d482h-ui <k4b59d482h@privaterelay.appleid.com>
2026-05-23 16:35:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from aiohttp import web
 
-# Налаштування логів
 logging.basicConfig(level=logging.INFO)
 
-# Новий токен твого бота
 BOT_TOKEN = "7963453350:AAG8lJAgSKULro8mb-Fm7QWu3wBJYWW9D6U"
 CHAT_ID = -1001780467253
 
@@ -27,14 +26,26 @@ async def send_daily_poll():
     except Exception as e:
         logging.error(f"Помилка відправки опитування: {e}")
 
+# Веб-сторінка, яку шукає Render
+async def handle(request):
+    return web.Response(text="Бот працює успішно!")
+
 async def main():
     scheduler = AsyncIOScheduler()
     
-    # ТЕСТОВИЙ ЧАС: 13:25 за сервером = 16:25 за Києвом
-    scheduler.add_job(send_daily_poll, "cron", hour=13, minute=25)
+    # ТЕСТ НА 16:45 (16:45 - 3 години = 13:45)
+    scheduler.add_job(send_daily_poll, "cron", hour=13, minute=45)
     
     scheduler.start()
-    logging.info("Планувальник успішно запущено!")
+    logging.info("Планувальк успішно запущено!")
+    
+    # Піднімаємо сервер на порту 10000 для Render
+    app = web.Application()
+    app.router.add_get('/', handle)
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, '0.0.0.0', 10000)
+    await site.start()
     
     # Запуск бота
     await dp.start_polling(bot)
